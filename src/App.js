481024130f2a7da5bce9397b723e4efeb2cc0cdb
Kiet <ducvub10@gmail.com>
2023-07-03 10:26:57 +0700

    Lecture 57

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import {useState} from 'react';
+import AnimalShow from './AnimalShow';
+import './App.css';
+
+function getRandomAnimal(value){
+    const animals = ['bird', 'cow', 'cat', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
+// function useState(defaultValue){
+//     return{
+//         yourState: defaultValue,
+//         yourSetter: () => {}
+//     };
+// }
 function App(){
+    const [animals, setAnimals] = useState([]);
+
+    const renderedAnimals = animals.map((animal, index)=>{
+        return <AnimalShow type={animal} key={index}/>
+    });
+
     const handleClick = () => {
-        console.log('Button was clicked!')
+       setAnimals([...animals, getRandomAnimal()])
     };
 
-    return <div>
-        <button onClick={handleClick}>Add Animal
+    return <div className = "app">
+        <button onClick = {handleClick}
+        >
+        Add Animal
         </button>
+        <div>{renderedAnimals}</div>
     </div>
 }
+
 
 export default App;
